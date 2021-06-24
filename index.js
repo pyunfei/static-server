5f3f6118843fbbc49d1fc2ffdcdf9f58e254fefd
彭云飞 <pengyf02@mingyuanyun.com>
2021-06-24 17:35:02 +0800

    feat: :lock: 新增默认路径展示默认文件

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const Koa = require('koa')
+const Router = require("koa-router")
+const htmlRender = require("koa-html-render")
 const path = require('path')
 const content = require('./util/content')
 const mimes = require('./util/mimes')
 
 const app = new Koa()
+const router = new Router()
 
 // 静态资源目录对于相对入口文件index.js的路径
 const staticPath = './static'
@@ -14,6 +17,13 @@ function parseMime(url) {
   extName = extName ? extName.slice(1) : 'unknown'
   return mimes[extName]
 }
+
+app.use(htmlRender())
+app.use(router.routes()).use(router.allowedMethods())
+
+router.get("/",async ctx=>{
+  await ctx.html('index.html')
+})
 
 app.use(async (ctx) => {
   // 静态资源目录在本地的绝对路径
@@ -27,16 +37,17 @@ app.use(async (ctx) => {
     ctx.type = _mime
   }
   // 输出静态资源内容
-  ctx.res.writeHead(200)
-  ctx.res.write(_content, 'binary')
-  ctx.res.end()
-  // if (_mime && _mime.indexOf('image/') >= 0) {
-  //   // 如果是图片，则用node原生res，输出二进制数据
-  // } else {
-  //   // 其他则输出文本 不允许暴露访问目录
-  //   ctx.body = { error: "Document not found" }
-  // }
+  if (_mime && _mime.indexOf('image/') >= 0) {
+    // 如果是图片，则用node原生res，输出二进制数据
+    ctx.res.writeHead(200)
+    ctx.res.write(_content, 'binary')
+    ctx.res.end()
+  } else {
+    // 其他则输出文本 不允许暴露访问目录
+    ctx.body = { error: "Document not found", url: ctx.url }
+  }
 })
+
 
 app.listen(3434)
 console.log(`static-server 🚀 http://localhost:3434 is starting at port 3434`)
